0f0f777ff58e3026e9e522a29c0a24f39c05eee4
ameliandev <adrianmgdev@gmail.com>
2014-09-29 11:22:27 +0100

    Create IE8Less_HTML5.js
    Crea compatibilidad de versiones de IE 8 y anteriores con la estructura HTML5

---
+++ JavaScript/IE8Less_HTML5.js
@@ -0,0 +1,12 @@
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script>
+  document.createElement('header');
+  document.createElement('section');
+  document.createElement('article');
+  document.createElement('aside');
+  document.createElement('nav');
+  document.createElement('footer');
+</script>
